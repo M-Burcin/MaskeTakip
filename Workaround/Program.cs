83415840ccc94f96ac111a1dd172aba70ce66186
M-Burcin <159180171+M-Burcin@users.noreply.github.com>
2024-02-12 22:14:37 +0300

    Add files via upload

---
+++ Workaround/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Business.Concrete;
+using Entities.Concrete;
+
+Person person1 = new Person();
+person1.FirstName = "Burçin";
+person1.LastName = "Aksu";
+person1.DateOfBirthYear = 1990;
+person1.NationalIdentity = 38362590556;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
